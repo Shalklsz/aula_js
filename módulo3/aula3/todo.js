10d88a8064c8a23681d564d4896393341d7c6e54
unknown <jefferson_carradore1@saojose.sc.senai.br>
2019-06-24 11:13:33 -0300

    aula3, módulo3/ Apenas um commit pois eu esqueci de dar antes e fizrofessor

---
+++ módulo3/aula3/todo.js
@@ -0,0 +1,7 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+console.log(listElement);
+console.log(inputElement);
+console.log(buttonElement);
