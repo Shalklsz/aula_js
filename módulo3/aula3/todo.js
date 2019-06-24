44a9cbabfe64f027bc96621e8bf02000a2e03c48
unknown <jefferson_carradore1@saojose.sc.senai.br>
2019-06-24 11:37:32 -0300

    criação do for e impressão de todo aula3, módulo3

---
+++ módulo3/aula3/todo.js
@@ -0,0 +1,14 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = [
+   'Fazer Café',
+   'Beber Gasolina',
+   'Jogar lixo no lixo'];
+
+   function renderTodos() {
+        for (toodo of todos) {
+            console.log(todo);
+        }
+    }
