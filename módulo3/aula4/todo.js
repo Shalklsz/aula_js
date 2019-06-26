3fbe12e96381a8bcc57b1519eb2ac42abfd7cb34
unknown <jefferson_carradore1@saojose.sc.senai.br>
2019-06-26 12:19:05 -0300

    inicio aula4, módulo3

---
+++ módulo3/aula4/todo.js
@@ -0,0 +1,21 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = [
+    'Fazer Café',
+    'Beber Gasolina',
+    'Jogar lixo no lixo'];
+
+function renderTodos() {
+    for (todo of todos) {
+        var todoElement = document.createAttribute('li');
+        var todoText = document.createTextNode(todo);
+    }
+}
+
+renderTodos()
+
+function addTodo() {
+    
+}
