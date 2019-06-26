5c57a7b6505e23905ebd65430d9ee095a01bbd99
Shalksz <ShalkszBot@hotmail.com>
2019-06-26 19:09:13 -0300

    criação de alternativa para erro na chamada dos todos

--- módulo3/aula5/todo.js
+++ módulo3/aula5/todo.js
@@ -2,7 +2,7 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var todos = JSON.parse(localStorage.getItem('list_todos'));
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 var todos = [
     'Fazer Café',
